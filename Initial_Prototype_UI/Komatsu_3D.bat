ec442276b1b7dad7769f71da1f9490ad2d7adea4
Shahab Sabahi <27753966+Shahabks@users.noreply.github.com>
2021-05-06 10:40:02 +0900

    Add files via upload

---
+++ Initial_Prototype_UI/Komatsu_3D.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\Python36\python.exe" "C:\Users\CINPC0075\Desktop\Repo-KCIN3D-reconstruction\For_UI_FS\Initial_Prototype_UI\src\consol_interactive_ALPHA_3.85.py"
+pause
+pushd %~dp0
+cscript C:\Users\CINPC0075\Desktop\Repo-KCIN3D-reconstruction\For_UI_FS\Initial_Prototype_UI\src\3DFObjects.vbs
