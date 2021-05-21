745db7e26b3dfd479dbdb6166aac1bef16da7b45
Shahab Sabahi <27753966+Shahabks@users.noreply.github.com>
2021-05-21 10:22:56 +0900

    Add files via upload

---
+++ KM_CN_3D_TIN_SF_PROTOTYPE/KU_3D.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Python36\python.exe" "C:\Users\CINPC0075\Desktop\KM_CN_3D_TIN_SF_PROTOTYPE\src\consol_interactive_ALPHA_3.96.py"
+pushd %~dp0
+cscript C:\Users\CINPC0075\Desktop\KM_CN_3D_TIN_SF_PROTOTYPE\src\3DFObjects.vbs
