381315f100a34bd688b1c8d4974ffa276cfb8fa1
Shahab Sabahi <27753966+Shahabks@users.noreply.github.com>
2021-05-21 10:22:20 +0900

    Delete Komatsu_3D.bat

--- KM_CN_3D_TIN_SF_PROTOTYPE/Komatsu_3D.bat
+++
@@ -1,4 +0,0 @@
-"C:\Program Files\Python36\python.exe" "C:\Users\CINPC0075\Desktop\Repo-KCIN3D-reconstruction\For_UI_FS\Initial_Prototype_UI\src\consol_interactive_ALPHA_3.96.py"
-pause
-pushd %~dp0
-cscript C:\Users\CINPC0075\Desktop\Repo-KCIN3D-reconstruction\For_UI_FS\Initial_Prototype_UI\src\3DFObjects.vbs
